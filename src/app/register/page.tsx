ddb982d29c4694661a95eb1b5517805129589f15
mdmasudrana5060 <ranam5060@gmail.com>
2025-06-30 09:32:44 +0600

    register page added

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,9 +1,117 @@
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  Link,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+
 const registerPage = () => {
   // react hook form and in react hook form we useregisterPatient
   return (
-    <div>
-      <h1>register registerPage</h1>
-    </div>
+    <Container
+      sx={{
+        padding: "50px",
+      }}
+    >
+      <Stack
+        sx={{
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box
+          sx={{
+            maxWidth: 600,
+            width: "100%",
+            boxShadow: 1,
+            borderRadius: 1,
+            p: 4,
+            textAlign: "center",
+          }}
+        >
+          <Stack
+            sx={{
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            {/* <Box>
+              <Image src={assets.} />
+            </Box> */}
+            <Box>
+              <Typography variant="h6" fontWeight={600}>
+                Patient Register
+              </Typography>
+            </Box>
+          </Stack>
+          <Box>
+            <form>
+              <Grid container spacing={2} my={1}>
+                <Grid size={12}>
+                  <TextField
+                    label="Name"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                  ></TextField>
+                </Grid>
+                <Grid size={6}>
+                  <TextField
+                    label="Email"
+                    type="email"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                  ></TextField>
+                </Grid>
+                <Grid size={6}>
+                  <TextField
+                    label="Password"
+                    type="password"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                  ></TextField>
+                </Grid>
+                <Grid size={6}>
+                  <TextField
+                    label="Contact No"
+                    type="tel"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                  ></TextField>
+                </Grid>
+                <Grid size={6}>
+                  <TextField
+                    label="Address"
+                    type="text"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                  ></TextField>
+                </Grid>
+              </Grid>
+              <Button
+                sx={{
+                  marging: "10px  0px",
+                }}
+                fullWidth={true}
+              >
+                Register
+              </Button>
+              <Typography component="p" fontWeight={600}>
+                Do you already have an account? <Link href="/login">Login</Link>
+              </Typography>
+            </form>
+          </Box>
+        </Box>
+      </Stack>
+    </Container>
   );
 };
 export default registerPage;
